15487a02b2322c26e60859b03cf4dfaae952c5a2
Andreas Kluth <mail@andreaskluth.net>
2013-05-11 23:02:45 +0200

    Removed outdated comment.

--- TempFileBenchmark/FileUtility.cs
+++ TempFileBenchmark/FileUtility.cs
@@ -8,12 +8,7 @@ namespace TempFileBenchmark
     {
       File.Delete( fileName );
     }
-
-    /// <summary>
-    ///   Reads a <see cref="byte" /> array from a file.
-    /// </summary>
-    /// <param name="fileName">Name of the file.</param>
-    /// <returns>the byte array</returns>
+    
     public string ReadFile( string fileName )
     {
       return File.ReadAllText( fileName );
